b093da3a32a7b3b8f9d2b1c66431ad4d853e3bbb
Takeshi Kamada <e232029t@gmail.com>
2013-02-28 22:08:42 +0900

    dotfilesにあるファイルのシンボリックリンクを自動生成するシェル

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOT_FILES=(.vimrc .tmux.conf .zshrc .gitconfig .gitignore)
+
+for file in ${DOT_FILES[@]}
+
+do
+  if [ -a $HOME/$file ]; then
+    echo "Skip! ($file is existed.)"
+  else
+    ln -s $HOME/dotfiles/$file $HOME/$file
+  fi
+done
